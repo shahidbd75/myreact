839912881c6104a6b8383fd7cf3d8d99e61c64d7
Shahid <abdul.baset@aci-bd.com>
2021-05-25 16:37:08 +0600

    feat: change custom context to react context

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,5 +1,8 @@
-import createContext from '../lib/Context';
+// import createContext from '../lib/Context';
+import React from 'react';
 
-const ThemeContext = createContext({ theme: 'light' });
+// const ThemeContext = createContext({ theme: 'light' });
+
+const ThemeContext = React.createContext();
 
 export default ThemeContext;
